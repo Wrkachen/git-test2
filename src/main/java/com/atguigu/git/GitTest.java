7052169aa53b0f4449ab26332db20a7608e2b03d
wrka <Cdec03@outlook.com>
2023-02-23 22:08:08 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author CYP
+ * @crate 2023-02-23 21:50
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
